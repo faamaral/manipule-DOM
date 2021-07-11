7c7b55a0e392aa50c23dde2fba79cd15cc21ac2a
faamaral <fabianoamaral445@gmail.com>
2021-07-11 15:35:10 -0300

    add print list function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,60 @@
+var title, author, link ;
+var count = 0;
+
+function printList(read) {
+  //checkEmptyList();
+  let list = document.querySelector('ul');
+  let itemList = document.createElement('li');
+  let div = document.createElement('div');
+  let btn_buy = document.createElement('a');
+  let span_buy = document.createElement('span');
+  span_buy.className+=" material_icons";
+  span_buy.textContent = "shopping_cart";
+  btn_buy.appendChild(span_buy);
+  btn_buy.className+=" btn_buy";
+  btn_buy.href=link;
+  /*for (let i = 0; i < title.length; i++) {
+    
+    
+  }*/
+  let text = document.createTextNode(`${title} - ${author}`);
+  if (read == '1') {
+    itemList.classList.add("lido");
+  }
+ 
+  itemList.appendChild(text);
+  itemList.appendChild(btn_buy);
+  
+  list.append(itemList);
+  
+  
+
+}
+
+
 function checkEmptyList () {
   if (!document.querySelector('ul').childNodes.length) {
     document.querySelector('ul').innerHTML = 'Adicione um livro abaixo!';
   }
 }
 
-const fillList = () => {
-  let list = document.querySelector('ul');
-  let form = document.forms[0];
-  let titleInput = form.querySelector('input[name="titulo"]');
-  let title = titleInput.value;
-  let authorInput = form.querySelector('input[name="autor"]');
-  let author = authorInput.value;
-  let linkInput = form.querySelector('input[name="link"]');
-  let link = linkInput.value;
-  let lidoInput = form.querySelector('input[name="lido"]:checked');
-  let lido = lidoInput.value;
-
-  let itemList = document.createElement('li');
-  itemList.appendChild(document.createTextNode(title));
-  list.insertBefore(itemList, null);
+function fillList() {
   
-  //console.log(title, author, link, lido);
+  let form = document.forms[0];
+  let titleInput = form.querySelector('input[name="titulo"]').value;
+  title=titleInput;
+  let authorInput = form.querySelector('input[name="autor"]').value;
+  author=authorInput;
+  let linkInput = form.querySelector('input[name="link"]').value;
+  link=linkInput;
+  let readInput = form.querySelector('input[name="lido"]:checked').value;
+
+  printList(readInput);
+ 
+
+  
+  
+  console.log(title, author, link, readInput);
 }
 
 checkEmptyList()
